21a72e0d5cd8601f3ce786bd343e78e7f3c63925
sotelo <jhonysotelo.1015@gmail.com>
2023-02-10 18:24:18 -0500

    Primera versión

--- test.py
+++ test.py
@@ -3,6 +3,10 @@ import conversor as cv
 
 if __name__ == '__main__':
     conversor = cv.Conversor()
+    print('->', conversor.dec_oct(100))
+    print('->', conversor.dec_bin(100))
+    print('->', conversor.dec_hex(100))
+
     print('->',conversor.hex_oct('AA12'))
     print('->', conversor.hex_dec('AA12'))
     print('->', conversor.hex_bin('AA12'))
